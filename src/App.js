bc1dfc2ca6c04e40c13b4bf12c191f2b9af3ae1d
mithlesh yadav <yadavmithlesh784@gmail.com>
2023-03-27 10:30:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+  const [number, setNumber] = useState(0);
+  const previousValue = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+      setData(results);
+      previousValue.current = number;
+    };
+    fetchData();
+  }, [number]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      {data.map((item, index) => {
+        return (
+          <>
+
+            <div className="card" key={index}>
+              <h2> Random User</h2>
+              <h1><img src={item.picture.large} alt="jh" /></h1>
+
+              <h3>
+                {item.name.title} {item.name.first} {item.name.last}
+              </h3>
+              <p style={{ textTransform: "capitalize" }}>
+                {item.gender} ( {item.dob.age} )
+              </p>
+              <p>
+                <strong>City :</strong>
+                {item.location.city} <strong>State :</strong>{" "}
+                {item.location.state} <strong>Country :</strong>
+                {item.location.country}
+              </p>
+              <p>&#9993; {item.email}</p>
+              <p>
+                &#128222; {item.phone} | {item.cell}
+              </p>
+
+              <button style={{ fontSize: '30px', backgroundColor: 'green', }} onClick={() => setNumber((previous) => previous + 1)}>
+                next
+              </button>
+            </div>
+          </>
+        );
+      })}
+    </>
   );
 }
-
-export default App;
